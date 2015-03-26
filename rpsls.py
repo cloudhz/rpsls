944dba9f7faf1a5da2379f6a8d362c47f07bd04c
cloudhz <cloudhz@winterfell.local>
2015-03-25 21:11:36 -0700

    Add weight random choice after 5 moves, move cpu pick before user input

--- rpsls.py
+++ rpsls.py
@@ -1,5 +1,6 @@
 import sys
 import random
+from bisect import bisect
 
 VALID_CHOICES = set(['rock', 'paper', 'scissors', 'lizard', 'spock'])
 WINNER_LOOKUPS = {
@@ -24,40 +25,82 @@ WINNER_LOOKUPS = {
         'lose' : ['lizard', 'paper']
     }
 }
+PLAYER_THROW_HISTORY = {
+    'rock': 0, 'paper': 0, 'scissors': 0, 'lizard': 0, 'spock': 0
+}
 SCORE = {
     'win' : 0,
     'lose': 0,
     'tie': 0
 }
 
-def update_score(result):
+def update_game(user_choice, result):
+    """
+    Update game state
+
+    @param string user_choice    user move
+    @param string result         end result of the game
+    """
+    PLAYER_THROW_HISTORY[user_choice] += 1
     SCORE[result] += 1
 
 def random_choice():
+    """
+    Picks a move at random
+    """
     return random.sample(VALID_CHOICES, 1)[0]
 
-def pick_winner(user_choice, random_choice):
+def weighted_choice():
+    """
+    Picks a move based on past turns (weighted choice) 
+    """
+    throws = PLAYER_THROW_HISTORY.keys()
+    total = 0
+    picks = []
+
+    for throw in PLAYER_THROW_HISTORY:
+        total += PLAYER_THROW_HISTORY[throw]
+        picks.append(total)
+
+    r = random.random() * picks[-1]
+    i = bisect(picks, r)
+
+    return random.sample(WINNER_LOOKUPS[k[i]]['lose'], 1)[0]
+
+def calculate_winner(user_choice, random_choice):
+    """
+    Calculates the winner
+
+    @param string user_choice    user move
+    @param string result         end result of the game
+    """
     if random_choice in WINNER_LOOKUPS[user_choice]['win']:
-        update_score('win')
+        update_game(user_choice, 'win')
         return 'You win!'
     elif random_choice in WINNER_LOOKUPS[user_choice]['lose']:
-        update_score('lose')
+        update_game(user_choice, 'lose')
         return 'The computer wins!'
     else:
-        update_score('tie')
+        update_game(user_choice, 'tie')
         return 'Draw!'
 
 def main():
+    turns = 0
     while True:
         try:
+            if turns > 5:
+                cpu_input = weighted_choice()
+            else:
+                cpu_input = random_choice()
+
             cmd_input = raw_input('Rock, Paper, Scissors, Lizard, Spock! What do you choose? ').lower()
             if cmd_input in VALID_CHOICES:
                 print "You chose...", cmd_input
-                cpu_input = random_choice()
                 print "The computer chose...", cpu_input 
-                print pick_winner(cmd_input, cpu_input)
+                print calculate_winner(cmd_input, cpu_input)
             else:
                 print "Please choose a valid move."
+            turns += 1
         except KeyboardInterrupt:
             print '\n'
             print 'Wins: ', SCORE['win'], 'Losses: ', SCORE['lose'], 'Ties: ', SCORE['tie']
