a7824561cf7f940f7a91ab640b6ca0936c1dc31e
cloudhz <cloudhz@winterfell.local>
2015-03-26 20:27:17 -0700

    Move program moves into seperate bots, refactor into classes

--- rpsls.py
+++ rpsls.py
@@ -25,85 +25,91 @@ WINNER_LOOKUPS = {
         'lose' : ['lizard', 'paper']
     }
 }
-PLAYER_THROW_HISTORY = {
-    'rock': 0, 'paper': 0, 'scissors': 0, 'lizard': 0, 'spock': 0
-}
-SCORE = {
-    'win' : 0,
-    'lose': 0,
-    'tie': 0
-}
 
-def update_game(user_choice, result):
-    """
-    Update game state
+class Game():
+    def __init__(self):
+        self.score = { 'win' : 0, 'lose': 0, 'tie': 0 }
+        self.player_throw_history = { 'rock': 0, 'paper': 0, 'scissors': 0, 'lizard': 0, 'spock': 0 }
 
-    @param string user_choice    user move
-    @param string result         end result of the game
-    """
-    PLAYER_THROW_HISTORY[user_choice] += 1
-    SCORE[result] += 1
+    def update_game(self, user_choice, result):
+        """
+        Update game state
 
-def random_choice():
-    """
-    Picks a move at random
-    """
-    return random.sample(VALID_CHOICES, 1)[0]
+        @param string user_choice    user move
+        @param string result         end result of the game
+        """
+        self.player_throw_history[user_choice] += 1
+        self.score[result] += 1
 
-def weighted_choice():
-    """
-    Picks a move based on past turns (weighted choice) 
-    """
-    throws = PLAYER_THROW_HISTORY.keys()
-    total = 0
-    picks = []
+    def calculate_winner(self, user_choice, random_choice):
+        """
+        Calculates the winner
 
-    for throw in PLAYER_THROW_HISTORY:
-        total += PLAYER_THROW_HISTORY[throw]
-        picks.append(total)
+        @param string user_choice    user move
+        @param string result         end result of the game
+        """
+        if random_choice in WINNER_LOOKUPS[user_choice]['win']:
+            self.update_game(user_choice, 'win')
+            return 'You win!'
+        elif random_choice in WINNER_LOOKUPS[user_choice]['lose']:
+            self.update_game(user_choice, 'lose')
+            return 'The computer wins!'
+        else:
+            self.update_game(user_choice, 'tie')
+            return 'Draw!'
 
-    r = random.random() * picks[-1]
-    i = bisect(picks, r)
+    def run(self, cpu_input):
+        cmd_input = raw_input('Rock, Paper, Scissors, Lizard, Spock! What do you choose? ').lower()
+        if cmd_input in VALID_CHOICES:
+            print "You chose...", cmd_input
+            print "The computer chose...", cpu_input
+            print self.calculate_winner(cmd_input, cpu_input)
+        else:
+            print "Please choose a valid move."
 
-    return random.sample(WINNER_LOOKUPS[k[i]]['lose'], 1)[0]
+class RandomGameBot():
+    def choice(self):
+        """
+        Picks a move at random
+        """
+        return random.sample(VALID_CHOICES, 1)[0]
 
-def calculate_winner(user_choice, random_choice):
-    """
-    Calculates the winner
+class WeightedGameBot():
+    def choice(self, player_throw_history):
+        """
+        Picks a move based on past turns (weighted choice)
+        """
+        throws = player_throw_history.keys()
+        total = 0
+        picks = []
 
-    @param string user_choice    user move
-    @param string result         end result of the game
-    """
-    if random_choice in WINNER_LOOKUPS[user_choice]['win']:
-        update_game(user_choice, 'win')
-        return 'You win!'
-    elif random_choice in WINNER_LOOKUPS[user_choice]['lose']:
-        update_game(user_choice, 'lose')
-        return 'The computer wins!'
-    else:
-        update_game(user_choice, 'tie')
-        return 'Draw!'
+        for throw in player_throw_history:
+            total += player_throw_history[throw]
+            picks.append(total)
+
+            r = random.random() * picks[-1]
+            i = bisect(picks, r)
+
+        return random.sample(WINNER_LOOKUPS[throws[i]]['lose'], 1)[0]
 
 def main():
     turns = 0
+    random_bot = RandomGameBot()
+    weighted_bot = WeightedGameBot()
+    game = Game()
+
     while True:
         try:
             if turns > 5:
-                cpu_input = weighted_choice()
+                cpu_input = weighted_bot.choice(game.player_throw_history)
             else:
-                cpu_input = random_choice()
-
-            cmd_input = raw_input('Rock, Paper, Scissors, Lizard, Spock! What do you choose? ').lower()
-            if cmd_input in VALID_CHOICES:
-                print "You chose...", cmd_input
-                print "The computer chose...", cpu_input 
-                print calculate_winner(cmd_input, cpu_input)
-            else:
-                print "Please choose a valid move."
+                cpu_input = random_bot.choice()
+            game.run(cpu_input)
             turns += 1
         except KeyboardInterrupt:
             print '\n'
-            print 'Wins: ', SCORE['win'], 'Losses: ', SCORE['lose'], 'Ties: ', SCORE['tie']
+            score = game.score
+            print 'Wins: ', score['win'], 'Losses: ', score['lose'], 'Ties: ', score['tie']
             break
 
 if __name__  == '__main__':
